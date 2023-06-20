3529ff569e49faca20ec23b9b2bd7a729b0e6d2f
jai <jk007rai@gmail.com>
2023-06-20 15:28:12 +0530

    Done with models and databases

--- market.py
+++ market.py
@@ -1,5 +1,5 @@
 # app = Flask(__name__) ------> refers to local python file that we are working with
-# @app.route('/')------> know as decorator. It says what url i am going to navigate and display you some HTML code
+# @app.route('/')------> known as decorator. It says what url i am going to navigate and display you some HTML code
 """
 To run the project follow the given commands:-
 1) open the project directory in cmd
@@ -26,8 +26,34 @@ To run the project follow the given commands:-
 """
 # jinja is a special web templating syntax that we are able to access through html b/c it is specially designed for python web frameworks
 
+"""
+open home.html
+
+Overall, the {% extends 'base.html' %} syntax allows you to create
+modular and reusable templates by inheriting the structure and content
+from a base template while still providing flexibility for customization
+"""
+# we are going to use a database to fetch data for our program for this we are goiing to install sqlalchemy
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'  ---> it is added so that flask recognizes our databse
+# 'SQLALCHEMY_DATABASE_URI' ----> It is key
+# URI --> uniform resource identifier
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+      return f'Item {self.name}'
 
 
 @app.route('/')
@@ -37,11 +63,15 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
+    """
+     items = [
         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
         {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
+    """
+    items = Item.query.all()
+
     return render_template('market.html', items=items)
 
 
